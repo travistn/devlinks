bda3fbad3fec5f9f373dd7cd100db5cb60570f40
travistn <travisn714@gmail.com>
2024-03-11 17:06:03 -0700

    Created a LinkCard component to add/save/remove a link

---
+++ components/LinkCard.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+import drag_and_drop_icon from '../public/assets/icon-drag-and-drop.svg';
+
+export default function LinkCard() {
+  return (
+    <div className='flex flex-col gap-3 bg-very-light-gray rounded-xl p-5 mt-6'>
+      <div className='w-full flex flex-row justify-between items-center'>
+        <div className='flex flex-row items-center gap-2'>
+          <Image src={drag_and_drop_icon} alt='drag-and-drop-icon' />
+          <h3 className='text-gray font-bold'>Link #1</h3>
+        </div>
+        <p className='text-gray'>Remove</p>
+      </div>
+    </div>
+  );
+}
