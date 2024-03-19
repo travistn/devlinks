db6d699401ae11e34d5f8d398eb8772e1b2441c5
travistn <travisn714@gmail.com>
2024-03-19 16:44:30 -0700

    The displayed selected platform will change when clicking another platform

--- components/PlatformSelect.tsx
+++ components/PlatformSelect.tsx
@@ -7,14 +7,15 @@ import data from '../data.json';
 
 export default function PlatformSelect() {
   const [selectIsOpen, setSelectIsOpen] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState(data[0]);
 
   return (
-    <Listbox>
+    <Listbox value={selectedPlatform} onChange={setSelectedPlatform}>
       <div className='relative'>
         <Listbox.Button className='w-full flex flex-row items-center bg-white border border-light-gray rounded-lg py-3 px-4 hover:shadow-[0_0px_32px_0_rgba(99,60,255,0.25)] hover:border hover:border-violet'>
           <div className='flex flex-row items-center gap-3'>
-            <Image src={data[0].image} alt='github-icon' width={16} height={16} />
-            <span className='text-dark-gray'>{data[0].name}</span>
+            <Image src={selectedPlatform.image} alt='github-icon' width={16} height={16} />
+            <span className='text-dark-gray'>{selectedPlatform.name}</span>
           </div>
           {!selectIsOpen ? (
             <ChevronDownIcon
@@ -32,7 +33,7 @@ export default function PlatformSelect() {
           {data.map((social) => (
             <Listbox.Option
               key={social.name}
-              value={social.name}
+              value={social}
               className='flex flex-row items-center gap-3 border-b border-light-gray pb-3 lg:hover:cursor-pointer'>
               <Image
                 src={social.image}
