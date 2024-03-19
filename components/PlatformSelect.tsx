285ec19abf0a34d9c42ca46f29954ae28474fb03
travistn <travisn714@gmail.com>
2024-03-19 16:19:36 -0700

    Added styling to the select menu such as spacing and hover color

--- components/PlatformSelect.tsx
+++ components/PlatformSelect.tsx
@@ -9,9 +9,9 @@ export default function PlatformSelect() {
   const [selectIsOpen, setSelectIsOpen] = useState(false);
 
   return (
-    <div className='bg-white border border-light-gray rounded-lg py-3 px-4'>
-      <Listbox>
-        <Listbox.Button className='w-full flex flex-row items-center'>
+    <Listbox>
+      <div className='relative'>
+        <Listbox.Button className='w-full flex flex-row items-center bg-white border border-light-gray rounded-lg py-3 px-4 hover:shadow-[0_0px_32px_0_rgba(99,60,255,0.25)] hover:border hover:border-violet'>
           <div className='flex flex-row items-center gap-3'>
             <Image src={data[0].image} alt='github-icon' width={16} height={16} />
             <span className='text-dark-gray'>{data[0].name}</span>
@@ -28,12 +28,12 @@ export default function PlatformSelect() {
             />
           )}
         </Listbox.Button>
-        <Listbox.Options>
+        <Listbox.Options className='absolute mt-4 bg-white rounded-lg border border-light-gray py-3 px-4 w-full shadow-[0_4px_32px_0_rgba(0,0,0,0.1)] flex flex-col gap-3'>
           {data.map((social) => (
             <Listbox.Option
               key={social.name}
               value={social.name}
-              className='flex flex-row items-center gap-3'>
+              className='flex flex-row items-center gap-3 border-b border-light-gray pb-3 lg:hover:cursor-pointer'>
               <Image
                 src={social.image}
                 alt={`${social.name}-icon`}
@@ -41,11 +41,11 @@ export default function PlatformSelect() {
                 width={16}
                 height={16}
               />
-              <span className='text-dark-gray'>{social.name}</span>
+              <span className='text-dark-gray lg:hover:text-violet'>{social.name}</span>
             </Listbox.Option>
           ))}
         </Listbox.Options>
-      </Listbox>
-    </div>
+      </div>
+    </Listbox>
   );
 }
