2cd35436f451e56ec754a7825d55f7e91098860c
travistn <travisn714@gmail.com>
2024-03-26 15:30:16 -0700

    Section to add a link will be toggled by clicking the Add new link button

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import Image from 'next/image';
 
 import illustration_image from '../public/assets/illustration-empty.svg';
 import illustration_image_mockup from '../public/assets/illustration-phone-mockup.svg';
+import LinkCard from './LinkCard';
 
 export default function GetStarted() {
+  const [linkToggle, setLinkToggle] = useState(false);
+
   return (
     <section className='p-4 lg:grid grid-cols-7 gap-6'>
-      <div className='max-lg:hidden bg-white p-6 w-full flex items-center justify-center rounded-lg col-span-3'>
-        <Image src={illustration_image_mockup} alt='illustration-image-mockup' className='' />
+      <div className='max-lg:hidden bg-white pt-[53.5px] pr-[24.5px] pb-[43.5px] pl-[23.5px] w-full flex items-center justify-center rounded-lg col-span-3'>
+        <Image src={illustration_image_mockup} alt='illustration-image-mockup' />
       </div>
       <div className='bg-white flex flex-col p-6 rounded-xl col-span-4 md:p-10'>
         <div className='flex flex-col gap-2'>
@@ -18,23 +22,29 @@ export default function GetStarted() {
             Add/edit/remove links below and then share all your profiles with the world!
           </p>
         </div>
-        <button className='rounded-lg py-[11px] px-[27px] border border-violet text-violet font-semibold mt-10 hover:bg-light-lavender'>
+        <button
+          className='rounded-lg py-[11px] px-[27px] border border-violet text-violet font-semibold mt-10 hover:bg-light-lavender'
+          onClick={() => (!linkToggle ? setLinkToggle((prevState) => !prevState) : '')}>
           + Add new link
         </button>
-        <div className='bg-very-light-gray mt-6 px-5 py-11 flex flex-col items-center gap-6 rounded-xl'>
-          <Image
-            src={illustration_image}
-            alt='illustration-image'
-            className='w-[124.8px] md:w-[249.5px]'
-          />
-          <h3 className='text-[24px] text-dark-gray font-bold md:text-[32px]'>
-            Let’s get you started
-          </h3>
-          <p className='text-gray text-center md:px-16'>
-            Use the “Add new link” button to get started. Once you have more than one link, you can
-            reorder and edit them. We’re here to help you share your profiles with everyone!
-          </p>
-        </div>
+        {!linkToggle ? (
+          <div className='bg-very-light-gray mt-6 px-5 py-11 flex flex-col items-center gap-6 rounded-xl'>
+            <Image
+              src={illustration_image}
+              alt='illustration-image'
+              className='w-[124.8px] md:w-[249.5px]'
+            />
+            <h3 className='text-[24px] text-dark-gray font-bold md:text-[32px]'>
+              Let’s get you started
+            </h3>
+            <p className='text-gray text-center md:px-16'>
+              Use the “Add new link” button to get started. Once you have more than one link, you
+              can reorder and edit them. We’re here to help you share your profiles with everyone!
+            </p>
+          </div>
+        ) : (
+          <LinkCard />
+        )}
         <div className='border border-light-gray mt-6 -mx-6 md:-mx-10' />
         <button className='mt-4 py-[11px] px-[27px] bg-violet/25 rounded-lg font-semibold text-white md:w-[91px] md:ml-auto lg:hover:bg-lavendar lg:hover:shadow-[0_4px_32px_0_rgba(99,60,250,0.25)]'>
           Save
