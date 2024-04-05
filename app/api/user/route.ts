bc11ded546f09462a16a09c174de8d0f218ac1b8
travistn <travisn714@gmail.com>
2024-04-04 18:28:50 -0700

    Defined User API route to retrieve users from MongoDB and return the users in a response

---
+++ app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+
+import { connectToDb } from '@/utils/mongodb';
+import User from '@/models/user';
+
+export const GET = async () => {
+  try {
+    await connectToDb();
+
+    const users = await User.find({});
+
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    return NextResponse.json('Failed to fetch users.', { status: 500 });
+  }
+};
