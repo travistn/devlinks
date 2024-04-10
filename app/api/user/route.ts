7aa91b6c020c6e92a1d30f81c96f9863b2cb36d4
travistn <travisn714@gmail.com>
2024-04-09 17:07:48 -0700

    Modified GET function to populate the links array with the Link model that have mathcing user ids

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -7,7 +7,7 @@ export const GET = async () => {
   try {
     await connectToDb();
 
-    const users = await User.find({});
+    const users = await User.find({}).populate({ path: 'links' });
 
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
