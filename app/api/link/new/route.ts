9b449a41875fef2b6189dea00bcb5dff04f97913
travistn <travisn714@gmail.com>
2024-04-09 17:06:54 -0700

    Modified POST function to inject link id into the links array in the User model when creating a new link

--- app/api/link/new/route.ts
+++ app/api/link/new/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { connectToDb } from '@/utils/mongodb';
 import Link from '@/models/link';
+import User from '@/models/user';
 
 export const POST = async (req: NextRequest) => {
   const { platform, url, user_id } = await req.json();
@@ -10,6 +11,12 @@ export const POST = async (req: NextRequest) => {
     await connectToDb();
 
     const newLink = await new Link({ platform, url, user_id }).save();
+
+    const existingUser = await User.findById(user_id);
+
+    existingUser.links.push(newLink._id);
+
+    await existingUser.save();
 
     return NextResponse.json(newLink, { status: 201 });
   } catch (error) {
